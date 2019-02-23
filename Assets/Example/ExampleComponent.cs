e8dafe851810d42411658a984475fe7640306ce9
Darkfafi <ik_ben_ramses@hotmail.com>
2019-02-23 16:41:30 +0100

    Created small / simple example in its own folder / namespace.

---
+++ Assets/Example/ExampleComponent.cs
@@ -0,0 +1,75 @@
+﻿using RDP.SaveLoadSystem;
+using UnityEngine;
+
+namespace RPD.SaveLoadSystem.Example
+{
+	public class ExampleComponent : MonoBehaviour, IStorageCapsule
+	{
+		public const string EXAMPLE_CAPSULE = "ExampleSaveFile";
+
+		public bool ClearSavesAtOnDestroy = false;
+		public bool RemoveFileOnClear = false;
+
+		private ReferenceBoy _referenceBoyA;
+		private ReferenceBoy _referenceBoyB;
+
+		private Storage _storage;
+
+		public string ID
+		{
+			get
+			{
+				return EXAMPLE_CAPSULE;
+			}
+		}
+
+		public void Awake()
+		{
+			_storage = new Storage("Saves/", Storage.EncodingType.Base64, this);
+			_storage.Load(EXAMPLE_CAPSULE); // Can be left empty to load all given capsules.
+		}
+
+		protected void Update()
+		{
+			if(Input.GetKeyDown(KeyCode.Space))
+			{
+				_referenceBoyA.Count();
+				_referenceBoyB.Count();
+			}
+		}
+
+		public void OnDestroy()
+		{
+			if(!ClearSavesAtOnDestroy)
+				_storage.Save(true, EXAMPLE_CAPSULE); // Can be left empty to save all given capsules.
+			else
+				_storage.FlushClear(RemoveFileOnClear, EXAMPLE_CAPSULE);
+		}
+
+		public void Save(IReferenceSaver saver)
+		{
+			saver.SaveRef("RefA", _referenceBoyA);
+			saver.SaveRef("RefB", _referenceBoyB);
+		}
+
+		public void Load(IReferenceLoader loader)
+		{
+			loader.LoadRef<ReferenceBoy>("RefA", (instance) => _referenceBoyA = instance);
+			loader.LoadRef<ReferenceBoy>("RefB", (instance) => _referenceBoyB = instance);
+		}
+
+		public void LoadingCompleted()
+		{
+			if(_referenceBoyA == null)
+			{
+				_referenceBoyA = new ReferenceBoy("Ref Boy A");
+			}
+
+			if(_referenceBoyB == null)
+			{
+				_referenceBoyB = new ReferenceBoy("Ref Boy B");
+				_referenceBoyB.CreateInnerReferenceBoy();
+			}
+		}
+	}
+}
---
+++ Assets/Example/ReferenceBoy.cs
@@ -0,0 +1,80 @@
+﻿using RDP.SaveLoadSystem;
+using UnityEngine;
+
+namespace RPD.SaveLoadSystem.Example
+{
+	public class ReferenceBoy : IRefereceSaveable
+	{
+		public string BoyName;
+		private ReferenceBoy _referenceBoy;
+		private int _count = 0;
+		private Vector2 _vecCool = new Vector2(1, 1);
+
+		public ReferenceBoy RefKid
+		{
+			get
+			{
+				return _referenceBoy;
+			}
+		}
+
+		public ReferenceBoy()
+		{
+			BoyName = "My Parents disliked me..";
+			_referenceBoy = null;
+		}
+
+		public ReferenceBoy(string name)
+		{
+			BoyName = name;
+		}
+
+		public void Count()
+		{
+			_count++;
+			_vecCool = Vector2.one * _count;
+
+			if(RefKid != null)
+			{
+				RefKid.Count();
+				RefKid.Count();
+			}
+
+			Debug.Log(ToString());
+		}
+
+		public void CreateInnerReferenceBoy()
+		{
+			_referenceBoy = new ReferenceBoy("Reference Inner");
+		}
+
+		public void Save(IReferenceSaver saver)
+		{
+			saver.SaveValue("name", BoyName);
+			saver.SaveValue("count", _count);
+			saver.SaveStruct("vec", _vecCool);
+			saver.SaveRef("ref", _referenceBoy, true);
+		}
+
+		public void Load(IReferenceLoader loader)
+		{
+			loader.LoadValue("name", out BoyName);
+			loader.LoadValue("count", out _count);
+			loader.LoadStruct("vec", out _vecCool);
+			loader.LoadRef<ReferenceBoy>("ref", (instance) =>
+			{
+				_referenceBoy = instance;
+			});
+		}
+
+		public void LoadingCompleted()
+		{
+			Debug.Log(ToString());
+		}
+
+		public override string ToString()
+		{
+			return "Name: " + BoyName + " | Count: " + _count + " | Vec: " + _vecCool;
+		}
+	}
+}
